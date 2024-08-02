92de373ab6998ed0183e82e48c8ee275ebff0ac4
Documenter.jl <documenter@juliadocs.github.io>
2024-08-02 18:03:18 +0000

    build based on ead1455

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Geometries","category":"page"},{"location":"#Geometries","page":"Home","title":"Geometries","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Geometries.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Geometries]","category":"page"}]
+}
